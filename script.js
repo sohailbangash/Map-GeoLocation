b4f71e93a39b55ac63d7eeea80d9187278e37ef4
sohailbangash <sohailbangashgithub2020.com>
2021-10-12 22:40:03 +0200

    working on validation inputs

--- script.js
+++ script.js
@@ -3,6 +3,57 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+
+
+
+class Workout {
+    date = new Date();
+    // id = (new Date + '').slice(-10)
+    id = (Date.now() + '').slice(-10)
+    constructor(coords, distance, duration) {
+        this.coords = coords;
+        this.distance = distance;
+        this.duration = duration;
+
+
+    }
+}
+
+class Running extends Workout {
+    constructor(coords, distance, duration, cadence) {
+        super(coords, distance, duration);
+        this.cadence = cadence;
+        this.calPace();
+    }
+    calPace() {
+        /* !!!:   min/km */
+        this.pace = this.duration / this.distance;
+        return this.pace;
+    }
+}
+
+class Cycling extends Workout {
+    constructor(coords, distance, duration, elavationGain) {
+        super(coords, distance, duration);
+        this.elavationGain = elavationGain;
+        this.calSpeed();
+    }
+    calSpeed() {
+        /* !!!:   km/hr */
+        this.Speed = this.distance / (this.distance / 60);
+        return this.pace;
+    }
+}
+
+// EXPERIMENT
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cycling1 = new Cycling([39, -12], 27, 95, 523);
+console.log(run1, cycling1);
+
+
+
+/// APPLICATION ARCHITECTURE
+
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -11,11 +62,11 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
 
-class App {#
-    map;#
-    mapEvent;
+let map, mapEvent;
+class App {
+  #map;
+  #mapEvent;
 
     constructor() {
         this._getPostion();
@@ -69,16 +120,41 @@ class App {#
 
     _newWorkout(e) {
         e.preventDefault();
+        const validInput = (...inputs) => inputs.every(inp => Number.isFinite(inp));
+        const allPostive = (...inputs) => inputs.every(inp => inp > 0)
 
-        // NOTE:  jshint ignore:  Clear input fields
-        inputType.value =
-            inputDuration.value =
-            inputElevation.value =
-            inputElevation.value =
-            '';
+        // Get data from
+        const type = inputType.value;
+        const distance = +inputDistance.value;
+        const duration = +inputDuration.value;
 
-        // console.log(mapEvent);
-        /* NOTE: display marker */
+
+        //  If working  running, create running object
+        if (type === 'running') {
+            const cadence = +inputCadence.value;
+
+            // Check if data is vaild
+            if (
+                // !Number.isFinite(distance) ||
+                // !Number.isFinite(duration) ||
+                // !Number.isFinite(cadence)
+
+                !validInput(distance, duration, cadence) || !allPostive(distance, duration, cadence)
+            ) return alert('Inputs have to be positive number!')
+        }
+
+        //  If  workout cycling,  create cycling oject
+        if (type === 'cycling') {
+            const elevation = +inputElevation.value;
+
+            // Check if data is vaild
+            if (!validInput(distance, duration, elevation) ||
+                !allPostive(distance, duration)
+            ) return alert('Inputs have to be positive number!')
+        }
+        // Add new object  to workout  array
+        // Render  workout  on map as marker
+
         const { lat, lng } = this.#mapEvent.latlng;
         L.marker([lat, lng])
             .addTo(this.#map)
@@ -93,6 +169,18 @@ class App {#
             )
             .setPopupContent('Workout')
             .openPopup();
+        //  Render workout  on list
+        // Hide form + Clear input fields
+
+        //NOTE:  jshint ignore:
+        inputType.value =
+            inputDuration.value =
+            inputElevation.value =
+            inputElevation.value =
+            '';
+        // console.log(mapEvent);
+
+
     }
 }
 
